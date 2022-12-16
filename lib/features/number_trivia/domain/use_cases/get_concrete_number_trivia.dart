306cf2ef456464b8b4af264a94c2b3d7824801ed
omeryilmaz2K <101820131+omeryilmaz2K@users.noreply.github.com>
2022-12-17 00:47:11 +0300

    GetConcreteNumberTrivia use case test completed at Test-Driven Development style.

--- lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
@@ -1,7 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture/core/error/failures.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia {
   const GetConcreteNumberTrivia(this.repository);
 
   final NumberTriviaRepository repository;
+
+  Future<Either<Failure, NumberTrivia>?> execute({
+    required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
 }
--- test/features/number_trivia/domain/use_cases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/use_cases/get_concrete_number_trivia_test.dart
@@ -1,3 +1,5 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,11 +9,29 @@ class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
 void main() {
-  GetConcreteNumberTrivia useCase;
-  MockNumberTriviaRepository mockNumberTriviaRepository;
+  GetConcreteNumberTrivia? useCase;
+  MockNumberTriviaRepository? mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    useCase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    useCase = GetConcreteNumberTrivia(mockNumberTriviaRepository!);
   });
+
+  const tNumber = 38;
+  const tNumberTrivia = NumberTrivia(text: 'test', number: 38);
+
+  test(
+    'should get trivia for the number from the repository',
+    () async {
+      // arrange
+      when(mockNumberTriviaRepository?.getConcreteNumberTrivia(any))
+      .thenAnswer((_) async => const Right(tNumberTrivia));
+      // act
+      final result = await useCase?.execute(number: tNumber);
+      // assert
+      expect(result, const Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository?.getConcreteNumberTrivia(tNumber));
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
 }
